a297e1625def59cc925aac1c3d3fc8b9687deb4f
himanshu <himanshu03vsk@gmail.com>
2022-10-14 23:23:43 +0530

    Deleted unwanted files and implemented logic

--- application.py
+++ application.py
@@ -1,19 +1,13 @@
 import os
-
-# from cs50 import SQL
+from dotenv import load_dotenv
+import openai
+load_dotenv()
+openai.api_key=os.getenv('API_KEY')
 from flask import Flask, flash, redirect, render_template, request, session, url_for
-# from flask_session import Session
-from tempfile import mkdtemp
-from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
-from werkzeug.security import check_password_hash, generate_password_hash
-
-# from helpers import apology, login_required, lookup, usd
 
 
-# PEOPLE_FOLDER = os.path.join('static', 'people_photo')
 
-
-# Configure application
+# Configure application 
 app = Flask(__name__)
 # app.config['UPLOAD_FOLDER'] = PEOPLE_FOLDER
 # Ensure templates are auto-reloaded
@@ -48,48 +42,59 @@ def index():
         argdict["gap"] = request.form.get('gap')
         argdict["fee"] = request.form.get('fee')
         argdict["sopfee"] = request.form.get('sopfee')
-        
-        return render_template("index.html",argdict)
-    else:
-        return render_template("index.html")
+        prompt = f'''
+        Generate statement of purpose because you got previously refused for visa.
+        using below information, explain the reasoning behind choosing the college and country and follow the format given in the example below
+        example: To,
+        The Visa Officer
+        High Commission of {argdict["tarcont"]} 
+        Write captial of {argdict["loc"]}
+        Subject: Statement of Purpose / Explanation regarding previous student visa refusal
+        Dear Sir/ Madam,
+        introduce yourself in brief
+        mention your academic skills and write your academic percentages 
+        mention what  is your family background and about parents what your father does what your mom is doing etc in detail
+        use the family information and relevent information as a reason for returning to {argdict["loc"]}
+        mention finances and how your parents will support you
+        mention you need to return to your home country due to family and current background
+        show proof that you have sufficient money
+        conclusion:
+        conclude the letter mentioning that you hope that you will definately return to {argdict["loc"]} 
 
-@app.route("/submit", methods=["GET","POST"])
-def submit():
-    """gets data from user, feeds it into the model to get prediction"""
-    if request.method == "POST":
-        # print(request.form)
-        year = int(request.form.get('year'))
-        age = 2021 - year
-        km_driven = int(request.form.get('km_driven'))
-        price = float(request.form.get('price'))
-        no_of_owners = int(request.form.get('no_of_owners'))
-        petrol = 0
-        diesel = 0
-        transmission = request.form.get('transmission')
-        if transmission == "on":
-            transmission_manual = 0
-        else:
-            transmission_manual = 1
-        seller_type = request.form.get('buyer_kind')
-        if seller_type == 'dealer':
-            seller_type_individual = 0
-        else:
-            seller_type_individual = 1
-        fuel = ['petrol','diesel','cng']
-        if request.form.get(fuel[0]) == 'on':
-            petrol = 1
-            diesel = 0
-            # fuel_type = fuel[0]
-        if request.form.get(fuel[1]) == 'on':
-            petrol = 0
-            diesel = 1
-            # fuel_type = fuel[1]
-        if request.form.get(fuel[2]) == 'on':
-            petrol = 0
-            diesel = 0
-            # fuel_type = fuel[2]
-        output = final_output(price, km_driven,  no_of_owners ,age, diesel, petrol, seller_type_individual, transmission_manual)
-        return render_template("submit.html", result=output)
+
+
+        Details:
+        Name- {argdict["name"]}
+        Date of birth- {argdict["dob"]}
+        Current location- {argdict["loc"]}
+        Contact Number- {argdict["contact"]}
+        Country for which applying- {argdict["tarcont"]}
+        College Name- {argdict["college"]}
+        Course- {argdict["course"]}
+        IELTS(Overall, Listening, Reading, Writing, Speaking)- {argdict["ielts"]}
+        Academic Percentage and graduation information- {argdict["academic"]}
+        Experience-{argdict["hobbiesexp"]}
+        Information about parents- {argdict["parinfo"]}
+        Any visa refusal in past- {argdict["visa"]}
+        Any GAP- {argdict["gap"]}
+        Anything relevent to course you are taking-{argdict["addinfo"]}
+        GIC and fee info-  {argdict["fee"]}
+        SOP fee paid- {argdict["sopfee"]}
+
+        SOP:
+'''
+        response = openai.Completion.create(model="text-davinci-002",
+                                            prompt=prompt,
+                                            temperature=0.92,
+                                            max_tokens=2500,
+                                            top_p=1,
+                                            frequency_penalty=0,
+                                            presence_penalty=0)
+
+        # print(response["choices"][0]["text"])
+        return render_template("index.html", response=response["choices"][0]["text"], resFlag=True)
+    else:
+        return render_template("index.html", resFlag=False)
 
 
 if __name__ == '__main__':
