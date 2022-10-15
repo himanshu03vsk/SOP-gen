31d13558a9b746c2a73de37e99c1427ff1fad26f
himanshu <himanshu03vsk@gmail.com>
2022-10-15 19:22:06 +0530

    Completed the project

--- application.py
+++ application.py
@@ -42,28 +42,62 @@ def index():
         argdict["gap"] = request.form.get('gap')
         argdict["fee"] = request.form.get('fee')
         argdict["sopfee"] = request.form.get('sopfee')
-        prompt = f'''
-        Generate statement of purpose because you got previously refused for visa.
-        using below information, explain the reasoning behind choosing the college and country and follow the format given in the example below
-        example: To,
+        prompt = f''' Write statement of purpose using the details and template given below -
+
+        template- To,
         The Visa Officer
         High Commission of {argdict["tarcont"]} 
-        Write captial of {argdict["loc"]}
+        Write captial of {argdict["loc"]} here
         Subject: Statement of Purpose / Explanation regarding previous student visa refusal
         Dear Sir/ Madam,
-        introduce yourself in brief
-        mention your academic skills and write your academic percentages 
-        mention what  is your family background and about parents what your father does what your mom is doing etc in detail
-        use the family information and relevent information as a reason for returning to {argdict["loc"]}
-        mention finances and how your parents will support you
-        mention you need to return to your home country due to family and current background
-        show proof that you have sufficient money
-        conclusion:
-        conclude the letter mentioning that you hope that you will definately return to {argdict["loc"]} 
+        I wish to provide the following additional information supporting my application for a student visa.
+
+        Introduce yourself along stating your date of birth and location and which program you applied to at what college
+        Write that you got rejected for visa when you applied previously and list the reasons in bullet points
+        Start with writing what program you have applied to and the eligibility criteria
+        write that you are going to explain the issues that were present in previous letter
+
+        Purpose of Visit and Reason to Return :
+        write why do you wish to visit {argdict["tarcont"]} and study the course you have chosen, link this to your parents and curiosity
+        Write how your parents encouraged and supported you regarding your studies and how the course has helped you
+        Mention your academic skills and write your academic percentages like what school you went how was your performance there and percentages, write what college you went to complete your bachelor degree, along with that mention any extracurricular activities you did.
+        Write why the {argdict["course"]} is your favourite and your aptitude for that.
+        Write what have you learned in your bachelors course and if it relates to the course you are planning to choose.
+        Write any internships or any kind of work experience you did relevant to the course.
+        Write to pursue the course you did IELTS and your marks in that.
+        Write the subjects offered by the course in {argdict["college"]}
+        Write why the course will be advantageous for you and write about job market for {argdict["course"]} in {argdict["loc"]}
+        Write if you have any gap
+
+        Reason to Return:
+        Write that you will return to India with your degree and how it will affect you in recruitment process, explain this in 1 paragraph
+        Write the job prospects of the course you chose in India and its history in your country write some historical data and stats about the job availability and salary packages and how it affects the GDP, explain this in 1 paragraph
+        Write how the course you chose will be crucial for india and mention its responsibility explain this in 1 paragraph
+        Write some average salary of a typical cs graduate from {argdict["tarcont"]}, explain this in 1 paragraph
+        Write reasons other than job prospects like family being your reason to return to India in bullet points 
+
+        Proof of Financial Funds:
+        Write your finances here and how your parents will support you, explain this in 1 paragraph
+
+        Language Abilites:
+        Write your ielts score here and your language skills
+
+        Why {argdict["tarcont"]} for education and why {argdict["college"]}, explain this in 1 paragraph?
+        Write about the international exposure you will get and communication skills, explain this in 1 paragraph
+        write various advantages you get by studying in {argdict["tarcont"]} explain this in 1 paragraph
+        write any course time related fact like how some country have shorter study duration, explain this in 1 paragraph
+        Write advantages of your college ,explain this in 1 paragraph
+
+        Conclusion:
+        conclude the letter mentioning that you have presented the necessary information and reasons of return in the document and resolved all the concerns of last visa officer. Write your sole purpose of visiting canada
+        write that you are looking for positive response
+
+        Sincerely,
+        {argdict["name"]}
+        
 
 
-
-        Details:
+        Details-
         Name- {argdict["name"]}
         Date of birth- {argdict["dob"]}
         Current location- {argdict["loc"]}
@@ -81,15 +115,15 @@ def index():
         GIC and fee info-  {argdict["fee"]}
         SOP fee paid- {argdict["sopfee"]}
 
-        SOP:
+        Statement of purpose-
 '''
         response = openai.Completion.create(model="text-davinci-002",
                                             prompt=prompt,
-                                            temperature=0.92,
-                                            max_tokens=2500,
-                                            top_p=1,
-                                            frequency_penalty=0,
-                                            presence_penalty=0)
+                                            temperature=1,
+                                            max_tokens=3000,
+                                            top_p=0.5,
+                                            frequency_penalty=0.74,
+                                            presence_penalty=0.36)
 
         # print(response["choices"][0]["text"])
         return render_template("index.html", response=response["choices"][0]["text"], resFlag=True)
@@ -99,5 +133,18 @@ def index():
 
 if __name__ == '__main__':
     app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
